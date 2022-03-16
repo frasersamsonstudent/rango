30c11b20da9b07fc165f4d643944508238cd84cc
Fraser Samson <2661506S@student.gla.ac.uk>
2022-03-16 15:21:33 +0000

    Completed chapter 6 exercises

--- populate_rango.py
+++ populate_rango.py
@@ -15,25 +15,25 @@ def populate():
 
     python_pages = [
     {'title': 'Official Python Tutorial',
-    'url':'http://docs.python.org/3/tutorial/'},
+    'url':'http://docs.python.org/3/tutorial/', 'views': 5},
     {'title':'How to Think like a Computer Scientist',
-    'url':'http://www.greenteapress.com/thinkpython/'},
+    'url':'http://www.greenteapress.com/thinkpython/', 'views': 15},
     {'title':'Learn Python in 10 Minutes',
-    'url':'http://www.korokithakis.net/tutorials/python/'} ]
+    'url':'http://www.korokithakis.net/tutorials/python/', 'views': 5} ]
 
     django_pages = [
     {'title':'Official Django Tutorial',
-    'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+    'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views': 4},
     {'title':'Django Rocks',
-    'url':'http://www.djangorocks.com/'},
+    'url':'http://www.djangorocks.com/', 'views': 25},
     {'title':'How to Tango with Django',
-    'url':'http://www.tangowithdjango.com/'} ]
+    'url':'http://www.tangowithdjango.com/', 'views': 30} ]
 
     other_pages = [
     {'title':'Bottle',
-    'url':'http://bottlepy.org/docs/dev/'},
+    'url':'http://bottlepy.org/docs/dev/', 'views': 100},
     {'title':'Flask',
-    'url':'http://flask.pocoo.org'} ]
+    'url':'http://flask.pocoo.org', 'views': 30} ]
 
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
     'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
@@ -49,7 +49,7 @@ def populate():
                     cat_data['views'] if 'views' in cat_data else 0,
                     cat_data['likes'] if 'likes' in cat_data else 0)
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'] if 'views' in p else 0)
 
     # Print out the categories we have added.
     for c in Category.objects.all():
--- rango/admin.py
+++ rango/admin.py
@@ -6,6 +6,12 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    # Update the registration to include this custom
+
+
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
